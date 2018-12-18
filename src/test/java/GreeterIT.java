eac670d87f51618bcb667cecea6bacc2fdde3539
Srini <svasan@protonmail.com>
2018-12-18 11:50:13 -0500

    Added an assert for the unit test.

--- src/test/java/GreeterIT.java
+++ src/test/java/GreeterIT.java
@@ -1,9 +1,11 @@
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class GreeterIT {
     @Test
     public void given_name_should_return_initials() {
         final String initials = new Greeter().getInitials("Mickey Mouse");
-        System.out.println(initials);
+        assertThat(initials).isEqualTo("MM");
     }
 }
